5a0b84efc2d37d8cfb6f3c8745670e8041adc481
Addicted <tanguypruvost67@gmail.com>
2023-11-23 19:32:53 +0100

    Ajout des seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(9)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory(6)->create();
+
+        User::factory()->has(
+                Post::factory(),
+         )->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
     }
 }
