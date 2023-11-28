6fa7ee84dd4a18917f0036e59ddc1e6812ac64ee
Addicted <tanguypruvost67@gmail.com>
2023-11-28 21:21:32 +0100

    Middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\Auth\LoginController;
 
@@ -17,11 +18,25 @@ use App\Http\Controllers\Api\Auth\LoginController;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
 
-Route::apiResource('posts', PostController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::middleware(['post'])->group(function () {
+        Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    });
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+});
+
+
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::apiResource('categories', CategoryController::class);
